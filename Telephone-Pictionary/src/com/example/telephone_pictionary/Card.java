e6157d70a05d4939f4bc4ea73668a3f050d2f889
lynnel <lynne.lammers@gmail.com>
2014-12-10 10:00:53 -0600

    Adding card class and filling out model

---
+++ Telephone-Pictionary/src/com/example/telephone_pictionary/Card.java
@@ -0,0 +1,17 @@
+package com.example.telephone_pictionary;
+
+import android.graphics.Bitmap;
+
+public class Card {
+	public enum Type {
+		IMAGE,
+		TEXT
+	}
+	public Bitmap m_image;
+	public Type m_type;
+	
+	Card (Bitmap image, Type type) {
+		m_image = image;
+		m_type = type;
+	}
+}
--- Telephone-Pictionary/src/com/example/telephone_pictionary/Model.java
+++ Telephone-Pictionary/src/com/example/telephone_pictionary/Model.java
@@ -7,37 +7,43 @@ import android.graphics.Bitmap;
 
 public class Model 
 {
-	private int numUsers;
-	private List<Bitmap> cards;
-	boolean isGameOver;
+	private int m_numUsers;
+	private List<Card> m_cards;
+	boolean m_isGameOver;
 	
 	public Model () 
 	{
-		numUsers = 0;
-		cards = new ArrayList<Bitmap>();
-		isGameOver = false;
+		m_numUsers = 0;
+		m_cards = new ArrayList<Card>();
+		m_isGameOver = false;
 	}
 	
 	public void setUsers(int users) 
 	{
-		numUsers = users;
+		m_numUsers = users;
 	}
 	
-	public void saveCard(Bitmap card) 
+	public boolean saveCard(Card card) 
 	{
-		cards.add(card);
+		m_cards.add(card);
+		if (m_cards.size() == m_numUsers) {
+			// game needs to end
+			return true;
+		}
+		// game continues
+		return false;
 	}
 	
-	public Bitmap getLastCard()
+	public Card getLastCard()
 	{
-		if (cards.isEmpty()) {
+		if (m_cards.isEmpty()) {
 			return null;
 		}
-		return cards.get(cards.size() - 1);
+		return m_cards.get(m_cards.size() - 1);
 	}
 	
-	public List<Bitmap> getAllCards() 
+	public List<Card> getAllCards() 
 	{
-		return cards;
+		return m_cards;
 	}
 }
