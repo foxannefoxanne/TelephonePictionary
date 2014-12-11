b2124f8df5a265b09c7075a7f9fcb5a8ef889942
lynnel <lynne.lammers@gmail.com>
2014-12-10 23:48:23 -0600

    Working on end game class

---
+++ Telephone-Pictionary/src/com/example/telephone_pictionary/EndGame.java
@@ -0,0 +1,17 @@
+package com.example.telephone_pictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class EndGame extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_end_game);
+	}
+	
+	
+}
