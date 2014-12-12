d3b9bc08c57e92eaafc10ab84d90c53402d79b36
foxannefoxanne <rox.j.calderon@gmail.com>
2014-12-11 23:55:05 -0600

    MAJOR CHANGES
    WOAH. Canvas now allows editing. Buttons that were on bottom are now on
    top. Buttons that were on top are now on  bottom. ABSOLUTE CHAOS

--- Telephone-Pictionary/src/com/example/telephone_pictionary/CanvasWriter.java
+++ Telephone-Pictionary/src/com/example/telephone_pictionary/CanvasWriter.java
@@ -6,6 +6,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,52 +17,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class CanvasWriter extends Activity {
-	private WritingTools writeTool; 
-
+	private EditText eText;
+	private Canvas writingCanvas;
+	private Bitmap writingBitmap; 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_canvas_writer);
 		
-		writeTool = (WritingTools)findViewById(R.id.writing);
-		writeTool.setDrawingCacheEnabled(true);
+
+		eText = (EditText) findViewById(R.id.writingeditor);
+		writingBitmap = Bitmap.createBitmap(50,50, Bitmap.Config.ARGB_8888);
+		writingCanvas = new Canvas(writingBitmap);
 
 	}
 
-	public void writeDialog(View view)
-	{
-		
-		TextView textView = new TextView(this); 
-		AlertDialog.Builder  textInput = new AlertDialog.Builder(this);
-		textInput.setTitle("Guess:");
-		LayoutInflater inflater = this.getLayoutInflater(); 
-		
-		// final View textRetriever = inflater.inflate(R.layout.text_input, null);
-		final EditText guess  = new EditText(this);
-		textInput.setView(guess);
-		// final EditText guess  = (EditText)textRetriever.findViewById(R.id.textEntry);
+	
 
-		textInput.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dinterface, int id) {
-				// EditText guess  = (EditText)textRetriever.findViewById(R.id.textEntry);
-				String guessString = guess.getText().toString();
-				writeTool.createText(guessString); 
-				dinterface.dismiss(); 
-			}
-		});
-		textInput.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dinterface, int id) {
-				dinterface.cancel();
-
-			}
-		});
-		textInput.create().show();				
-	}
 	
 	public void submitImage(View view) {
 		
-		writeTool.setDrawingCacheEnabled(true);
-   	    Bitmap bitmap = writeTool.getDrawingCache();
+
+		eText.setDrawingCacheEnabled(true);
+		eText.buildDrawingCache(); 
+		
+	
+   	    Bitmap bitmap = eText.getDrawingCache();
    	    Model model = Model.getInstance();
    	  
    	    Card cardStorage = new Card(bitmap,Card.Type.TEXT); 
